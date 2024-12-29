0df3614d1fbfc695b3d86958ef717cd2b55c0540
Salah Haddara <Salahhaddara02@gmail.com>
2024-12-29 19:36:10 +0200

    Define get route for /tokens and export the router

--- src/routes/designRoutes.js
+++ src/routes/designRoutes.js
@@ -1,4 +1,8 @@
 import express from 'express';
+import {getDesignTokens} from '../controllers/designTokenController.js';
 
 const router = express.Router();
 
+router.get('/tokens', getDesignTokens);
+
+export default router;
