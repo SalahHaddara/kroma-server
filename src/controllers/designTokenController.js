85c98487fe723441ef65b556194bc3d1b1c3ffd7
Salah Haddara <Salahhaddara02@gmail.com>
2024-12-29 19:23:18 +0200

    add: set up file structure and import modules

---
+++ src/controllers/designTokenController.js
@@ -0,0 +1,7 @@
+import {processImage} from '../services/imageService.js';
+import {designTokensStructure} from '../models/designTokens.js';
+import * as path from 'path';
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
