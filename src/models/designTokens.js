da268208a5bcadd10554dc7d43a8f8723c3fcaeb
Salah Haddara <Salahhaddara02@gmail.com>
2024-12-29 13:13:04 +0200

    add: color palette structure

---
+++ src/models/designTokens.js
@@ -0,0 +1,31 @@
+export const designTokensStructure = {
+    colors: [
+        [
+            "#FDF2E9",
+            "#FAD7A0",
+            "#F5B041",
+            "#EB984E",
+            "#E67E22",
+            "#CA6F1E",
+            "#A04000"
+        ],
+        [
+            "#FDEDEC",
+            "#F9EBEA",
+            "#F5CBA7",
+            "#F0B27A",
+            "#E59866",
+            "#DC7633",
+            "#D35400"
+        ],
+        [
+            "#FAFAFA",
+            "#F4F4F4",
+            "#E8E8E8",
+            "#D1D1D1",
+            "#9E9E9E",
+            "#616161",
+            "#424242"
+        ]
+    ],
+};
