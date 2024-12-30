720db37947e5a068e1e3372dbd395b1452c49303
Salah Haddara <Salahhaddara02@gmail.com>
2024-12-30 21:11:27 +0200

    Add design token structure

---
+++ src/config/constants.js
@@ -0,0 +1,15 @@
+export const DESIGN_TOKEN_PARTS = {
+    part1: {
+        colors: true,
+        typography: true,
+        buttons: true
+    },
+    part2: {
+        spacing: true,
+        icons: true,
+        quote: true,
+        alerts: true,
+        shadows: true,
+        borderRadius: true
+    }
+};
