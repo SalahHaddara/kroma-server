0004d36202e15515a9329a1061ff9b10c7252d20
Salah Haddara <Salahhaddara02@gmail.com>
2024-12-31 14:16:36 +0200

    update designTokenController.js to be more focused

--- src/controllers/designTokenController.js
+++ src/controllers/designTokenController.js
@@ -1,42 +1,8 @@
-import {processImage} from '../services/imageService.js';
-import {designTokensStructure} from '../models/designTokens.js';
-import * as path from 'path';
-import {fileURLToPath} from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import {generateDesignSystem} from '../services/designTokenService.js';
 
 export async function getDesignTokens(req, res) {
-    try {
-        const mainImage = await processImage(
-            path.join(__dirname, '../../public/images/main.jpg'),
-            800,
-            400
-        );
 
-        const smallImage1 = await processImage(
-            path.join(__dirname, '../../public/images/small1.jpg'),
-            400,
-            400
-        );
-
-        const smallImage2 = await processImage(
-            path.join(__dirname, '../../public/images/small2.jpg'),
-            400,
-            400
-        );
-
-        const designTokens = {
-            ...designTokensStructure,
-            inspirationImages: {
-                mainImage: {imageData: mainImage},
-                smallImage1: {imageData: smallImage1},
-                smallImage2: {imageData: smallImage2}
-            }
-        };
-
-        res.json(designTokens);
-    } catch (error) {
-        res.status(500).json({error: 'Failed to get design tokens'});
-    }
+    const userPrompt = req.query.prompt || req.body.prompt;
+    const designTokens = await generateDesignSystem(userPrompt);
+    res.json(designTokens);
 }
