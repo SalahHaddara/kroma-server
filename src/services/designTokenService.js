780bc02f4c1c3c3e45a4abd5256b604996ddd3d5
Salah Haddara <Salahhaddara02@gmail.com>
2024-12-31 14:00:48 +0200

    Add validation login

--- src/services/designTokenService.js
+++ src/services/designTokenService.js
@@ -1,6 +1,13 @@
+import {generateCompleteDesignTokens} from './openaiService.js';
+import {validateStructure, validatePrompt} from './validationService.js';
+import {designTokensStructure} from '../models/designTokens.js';
+import {processImage} from './imageService.js';
 import * as path from 'path';
 import {fileURLToPath} from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+export async function generateDesignSystem(prompt) {
+    validatePrompt(prompt);
+}
