5756632504063e31058266cd177dd30c094913eb
Salah Haddara <Salahhaddara02@gmail.com>
2024-12-31 14:36:21 +0200

    Add handing for bad request error

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,4 +1,9 @@
+import {ValidationError} from '../services/validationService.js';
+
 export function errorHandler(err, req, res, next) {
     console.error(err.stack);
 
+    if (err instanceof ValidationError) {
+        return res.status(400).json({error: err.message});
+    }
 }
