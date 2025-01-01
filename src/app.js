bde13dcd3fff93df6d49341f06c6d94c42870c82
Salah Haddara <Salahhaddara02@gmail.com>
2025-01-01 13:49:00 +0200

    Add errorHandler middleware

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import cors from 'cors';
 import designRoutes from './routes/designRoutes.js';
+import {errorHandler} from './middleware/errorHandler.js';
 
 const app = express();
+
 app.use(cors());
+app.use(express.json());
 
 app.use('/get-design', designRoutes);
+
+app.use(errorHandler);
 
 export default app;
