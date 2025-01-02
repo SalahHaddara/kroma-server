ac8426e1569f657c940b35c6bb616dab80550995
Salah Haddara <Salahhaddara02@gmail.com>
2025-01-02 17:32:08 +0200

    Call the image generation function

--- src/services/designTokenService.js
+++ src/services/designTokenService.js
@@ -1,4 +1,4 @@
-import {generateCompleteDesignTokens} from './aiService.js';
+import {generateCompleteDesignTokens, generateInspirationImages} from './aiService.js';
 import {validateStructure, validatePrompt} from './validationService.js';
 import {designTokensStructure} from '../models/designTokens.js';
 import {processImage} from './imageService.js';
@@ -11,7 +11,10 @@ const __dirname = path.dirname(__filename);
 export async function generateDesignSystem(prompt) {
     validatePrompt(prompt);
 
-    const {part1Response, part2Response} = await generateCompleteDesignTokens(prompt);
+    const [tokenResponse, inspirationImages] = await Promise.all([
+        generateCompleteDesignTokens(prompt),
+        generateInspirationImages(prompt)
+    ]);
 
     const combined = {
         ...JSON.parse(part1Response),
@@ -20,18 +23,8 @@ export async function generateDesignSystem(prompt) {
 
     validateStructure(combined, designTokensStructure);
 
-    const [mainImage, smallImage1, smallImage2] = await Promise.all([
-        processImage(path.join(__dirname, '../../public/images/main.jpg'), 800, 400),
-        processImage(path.join(__dirname, '../../public/images/small1.jpg'), 400, 400),
-        processImage(path.join(__dirname, '../../public/images/small2.jpg'), 400, 400)
-    ]);
-
     return {
         ...combined,
-        inspirationImages: {
-            mainImage: {imageData: mainImage},
-            smallImage1: {imageData: smallImage1},
-            smallImage2: {imageData: smallImage2}
-        }
+
     };
 }
