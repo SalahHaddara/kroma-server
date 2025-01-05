dddd953748d46aa533ed51bf624c58cdcd4d92bc
Salah Haddara <Salahhaddara02@gmail.com>
2025-01-05 23:51:42 +0200

    Handle missing token

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,5 +1,7 @@
 export const protect = async (req, res, next) => {
-
     const token = req.headers.authorization?.split(' ')[1];
 
+    if (!token) {
+        return res.status(401).json({message: 'Please log in to access this resource'});
+    }
 };
