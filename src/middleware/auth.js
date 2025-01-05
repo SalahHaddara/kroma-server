753df85eca6af98baa8551fd9cdbd57efd250756
Salah Haddara <Salahhaddara02@gmail.com>
2025-01-05 23:53:21 +0200

    Verify token

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,7 +1,12 @@
+import jwt from 'jsonwebtoken';
+import {promisify} from 'util';
+
 export const protect = async (req, res, next) => {
     const token = req.headers.authorization?.split(' ')[1];
 
     if (!token) {
         return res.status(401).json({message: 'Please log in to access this resource'});
     }
+
+    const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
 };
