c7262e1c0b8e73d921e63de7a821f60385197bb3
Salah Haddara <Salahhaddara02@gmail.com>
2025-01-06 00:01:24 +0200

    Handle invalid token

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,20 +3,23 @@ import {promisify} from 'util';
 import User from '../models/User.js';
 
 export const protect = async (req, res, next) => {
-    const token = req.headers.authorization?.split(' ')[1];
+    try {
+        const token = req.headers.authorization?.split(' ')[1];
 
-    if (!token) {
-        return res.status(401).json({message: 'Please log in to access this resource'});
+        if (!token) {
+            return res.status(401).json({message: 'Please log in to access this resource'});
+        }
+
+        const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+        const user = await User.findById(decoded.id);
+        if (!user) {
+            return res.status(401).json({message: 'User no longer exists'});
+        }
+
+        req.user = user;
+        next();
+    } catch (error) {
+        res.status(401).json({message: 'Invalid token'});
     }
-
-    const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
-
-    const user = await User.findById(decoded.id);
-    if (!user) {
-        return res.status(401).json({message: 'User no longer exists'});
-    }
-
-    req.user = user;
-    next();
-    
 };
