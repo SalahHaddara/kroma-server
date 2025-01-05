900d199d9c35f7d6450af9cd467da1c5e41a1bab
Salah Haddara <Salahhaddara02@gmail.com>
2025-01-05 23:18:13 +0200

    Hash password only if modified

--- src/models/User.js
+++ src/models/User.js
@@ -28,6 +28,8 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.pre('save', async function (next) {
-    this.password = await bcrypt.hash(this.password, 10);
+    if (this.isModified('password')) {
+        this.password = await bcrypt.hash(this.password, 12);
+    }
     next();
 })
