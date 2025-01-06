b80e5c47e59425b0beee05fa93ffea4e40c5b53b
Salah Haddara <Salahhaddara02@gmail.com>
2025-01-06 11:50:31 +0200

    Add signToken function for JWT generation

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,3 +1,10 @@
 import {OAuth2Client} from 'google-auth-library';
+import jwt from "jsonwebtoken";
 
 const googleClient = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+
+const signToken = (id) => {
+    return jwt.sign({id}, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN
+    });
+}
