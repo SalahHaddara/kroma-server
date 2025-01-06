0f45588e119ca636dc4909bac1550c506794de14
Salah Haddara <Salahhaddara02@gmail.com>
2025-01-06 12:53:07 +0200

    Add routes for each auth method

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,3 +1,9 @@
 import express from 'express';
 import {signup, login, googleAuth, githubAuth} from '../controllers/authController.js';
 
+const router = express.Router();
+
+router.post('/signup', signup);
+router.post('/login', login);
+router.post('/google', googleAuth);
+router.post('/github', githubAuth);
