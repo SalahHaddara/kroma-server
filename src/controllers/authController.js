29781b3e0e859ddf1887906ea5fe26c7bd2b2e9f
Salah Haddara <Salahhaddara02@gmail.com>
2025-01-06 11:55:54 +0200

    Handle existing user

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,6 @@
 import {OAuth2Client} from 'google-auth-library';
 import jwt from "jsonwebtoken";
+import User from "../models/User.js";
 
 const googleClient = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 
@@ -11,6 +12,11 @@ const signToken = (id) => {
 
 export const signup = async (req, res) => {
     const {fullName, email, password} = req.body;
+
+    const existingUser = await User.findOne({email});
+    if (existingUser) {
+        return res.status(400).json({message: 'Email already exist'});
+    }
 
 
 }
