7a177d4acf657733f40a7d5eb2ea7b218fc87885
Salah Haddara <Salahhaddara02@gmail.com>
2025-01-07 15:54:55 +0200

    Update app to include auth middleware

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import designRoutes from './routes/designRoutes.js';
 import {errorHandler} from './middleware/errorHandler.js';
+import authRoutes from "./routes/authRoutes.js";
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/get-design', designRoutes);
+app.use('/auth', authRoutes);
 
 app.use(errorHandler);
 
