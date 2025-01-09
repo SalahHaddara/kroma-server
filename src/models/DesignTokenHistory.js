62eddb65ea53ae8b75bd9186ab11739fc5ed6133
Salah Haddara <Salahhaddara02@gmail.com>
2025-01-09 14:34:49 +0200

    Add user to the schema

--- src/models/DesignTokenHistory.js
+++ src/models/DesignTokenHistory.js
@@ -1,6 +1,12 @@
 import mongoose from 'mongoose';
 
-const designTokenHistorySchema = new mongoose.Schema({});
+const designTokenHistorySchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+});
 
 
 const DesignTokenHistory = mongoose.model('DesignTokenHistory', designTokenHistorySchema);
