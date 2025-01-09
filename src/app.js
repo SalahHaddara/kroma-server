59e8e17350521ec360b56dd27c0e060ffeecc147
Salah Haddara <Salahhaddara02@gmail.com>
2025-01-09 17:47:31 +0200

    Add History router in the app.js

--- src/app.js
+++ src/app.js
@@ -3,14 +3,17 @@ import cors from 'cors';
 import designRoutes from './routes/designRoutes.js';
 import {errorHandler} from './middleware/errorHandler.js';
 import authRoutes from "./routes/authRoutes.js";
+import histroyRoutes from "./routes/histroyRoutes.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+express.urlencoded()
 
 app.use('/get-design', designRoutes);
 app.use('/auth', authRoutes);
+app.use('/api/history', histroyRoutes);
 
 app.use(errorHandler);
 
