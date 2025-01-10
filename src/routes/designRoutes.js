3b5fc9f89787c5f9b02705658293483cf507f522
Salah Haddara <Salahhaddara02@gmail.com>
2025-01-10 22:10:28 +0200

    Add a new endpoint to get the latest design system

--- src/routes/designRoutes.js
+++ src/routes/designRoutes.js
@@ -9,14 +9,8 @@ router.use(protect);
 router.get('/tokens', getDesignTokens);
 router.post('/tokens', getDesignTokens);
 
-router.get('/test-tokens', (req, res) => {
-    const staticPrompt = "A Website selling plants.";
+router.get('/latest-tokens', async (req, res) => {
 
-    // Add the static prompt to the request object
-    req.query.prompt = staticPrompt;
-
-    // Call the existing controller
-    getDesignTokens(req, res);
 });
 
 export default router;
