8c6558f03c464816abeff3f86f20b671c5b14f2f
Salah Haddara <Salahhaddara02@gmail.com>
2025-01-11 11:19:41 +0200

    Add save-design endpoint

--- src/routes/designRoutes.js
+++ src/routes/designRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {getDesignTokens, getLatestDesignTokens} from '../controllers/designTokenController.js';
+import {getDesignTokens, getLatestDesignTokens, saveDesignSVG} from '../controllers/designTokenController.js';
 import {protect} from "../middleware/auth.js";
 
 const router = express.Router();
@@ -9,5 +9,7 @@ router.use(protect);
 router.get('/tokens', getDesignTokens);
 router.post('/tokens', getDesignTokens);
 router.get('/latest-tokens', getLatestDesignTokens);
+router.post('/save-design', protect, saveDesignSVG);
+
 
 export default router;
