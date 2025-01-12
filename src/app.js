5eba9f7a37142bb28e4f254d758dbca0a7e2e31c
Salah Haddara <Salahhaddara02@gmail.com>
2025-01-12 14:38:02 +0200

    Update cors

--- src/app.js
+++ src/app.js
@@ -11,7 +11,8 @@ app.use(cors({
     origin: ['http://localhost:5173', 'null'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    exposedHeaders: ['Content-Type']
 }));
 
 app.options('*', cors());
