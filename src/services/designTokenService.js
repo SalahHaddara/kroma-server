f19e2e3155cc535e35a7948eddf852ed25a453f3
Salah Haddara <Salahhaddara02@gmail.com>
2025-01-12 22:43:01 +0200

    Remove unused import

--- src/services/designTokenService.js
+++ src/services/designTokenService.js
@@ -1,7 +1,6 @@
 import {generateCompleteDesignTokens, generateInspirationImages} from './aiService.js';
 import {validateStructure, validatePrompt} from './validationService.js';
 import {designTokensStructure} from '../models/designTokens.js';
-import {processImage} from './imageService.js';
 import * as path from 'path';
 import {fileURLToPath} from 'url';
 
