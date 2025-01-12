333bcfabf2dc46058c54e4f86baee0031c4d0556
Salah Haddara <Salahhaddara02@gmail.com>
2025-01-12 14:38:33 +0200

    Update design routes to handle file upload

--- src/routes/designRoutes.js
+++ src/routes/designRoutes.js
@@ -1,15 +1,20 @@
 import express from 'express';
-import {getDesignTokens, getLatestDesignTokens, saveDesignSVG} from '../controllers/designTokenController.js';
+import {
+    getDesignTokens,
+    getLatestDesignTokens,
+    saveDesignPNG
+} from '../controllers/designTokenController.js';
 import {protect} from "../middleware/auth.js";
+import fileUpload from 'express-fileupload';
 
 const router = express.Router();
 
 router.use(protect);
+router.use(fileUpload());
 
 router.get('/tokens', getDesignTokens);
 router.post('/tokens', getDesignTokens);
 router.get('/latest-tokens', getLatestDesignTokens);
-router.post('/save-design', protect, saveDesignSVG);
-
+router.post('/save-design', protect, saveDesignPNG);
 
 export default router;
