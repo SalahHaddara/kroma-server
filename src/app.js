b713ed24cdf1752f0f92fa06f2c0c7ca76cca406
Salah Haddara <Salahhaddara02@gmail.com>
2025-01-14 19:14:12 +0200

    Add analysis routes file to the app

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ import designRoutes from './routes/designRoutes.js';
 import {errorHandler} from './middleware/errorHandler.js';
 import authRoutes from "./routes/authRoutes.js";
 import histroyRoutes from "./routes/histroyRoutes.js";
+import analysisRoutes from "./routes/analysisRoutes.js";
+import fileUpload from "express-fileupload";
 
 const app = express();
 
@@ -18,12 +20,22 @@ app.use(cors({
 app.options('*', cors());
 
 app.use(express.json());
+app.use(fileUpload({
+    limits: {fileSize: 5 * 1024 * 1024},
+    abortOnLimit: true,
+    responseOnLimit: "File size limit exceeded (5MB)",
+    debug: true,
+    useTempFiles: false,
+    preserveExtension: true,
+    safeFileNames: true
+}));
 app.get('/test', (req, res) => {
     res.json({message: 'API working'});
 });
 app.use('/get-design', designRoutes);
 app.use('/auth', authRoutes);
 app.use('/api/history', histroyRoutes);
+app.use('/analysis', analysisRoutes);
 
 app.use(errorHandler);
 
