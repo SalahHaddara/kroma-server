56ed54cf42bfc294f61c16980a0427524c59b5be
Salah Haddara <Salahhaddara02@gmail.com>
2025-01-15 10:30:10 +0200

    Add the analysis routes middlware

--- src/routes/analysisRoutes.js
+++ src/routes/analysisRoutes.js
@@ -3,14 +3,24 @@ import {protect} from '../middleware/auth.js';
 import {
     analyzeDesign,
 } from '../controllers/designAnalysisController.js';
+import fileUpload from "express-fileupload";
 
 const router = express.Router();
 
-//authentication middleware
+// Apply authentication middleware
 router.use(protect);
 
+const fileUploadMiddleware = fileUpload({
+    limits: {fileSize: 5 * 1024 * 1024}, // 5MB max file size
+    abortOnLimit: true,
+    responseOnLimit: "File size limit exceeded (5MB)",
+    debug: true,
+    useTempFiles: false,
+    preserveExtension: true,
+    safeFileNames: true
+});
 // File upload route
-router.post('/analyze', analyzeDesign);
+router.post('/analyze', fileUploadMiddleware, analyzeDesign);
 // router.get('/history', getAnalysisHistory);
 // router.get('/:id', getAnalysisById);
 // router.delete('/:id', deleteAnalysisById);
